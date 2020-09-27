3613dfe9a6f7ed6570251c1d1148bd4dbe283e64
Navel Development <contact@navel.dev>
2020-09-28 00:31:17 +0200

    Update Application.php

--- application/Foundation/Application.php
+++ application/Foundation/Application.php
@@ -6,14 +6,39 @@ use Navel\Foundation\Container\Container;
 
 class Application extends Container
 {
+    /**
+     * The framework's version
+     *
+     * @var string
+     */
     protected $version = "2.0.0";
 
+    /**
+     * The application's base directory
+     *
+     * @var string
+     */
     protected $base_dir;
 
+    /**
+     * Wether the application is booted or not.
+     *
+     * @var boolval
+     */
     protected $booted = false;
 
+    /**
+     * The applications service providers
+     *
+     * @var array
+     */
     protected $serviceProviders;
 
+    /**
+     * The constructor function
+     *
+     * @param string $base_dir The applications base dir
+     */
     public function __construct( $base_dir = null )
     {
         if ($base_dir) {
@@ -25,21 +50,43 @@ class Application extends Container
         $this->registerBaseServiceProviders();
     }
 
+    /**
+     * [boot description]
+     *
+     * @return [type] [description]
+     */
     public function boot()
     {
         $this->booted = true;
     }
 
+    /**
+     * [registerBaseBindings description]
+     *
+     * @return [type] [description]
+     */
     private function registerBaseBindings()
     {
         $this->instance( 'app', $this );
     }
 
+    /**
+     * [registerBaseServiceProviders description]
+     *
+     * @return [type] [description]
+     */
     private function registerBaseServiceProviders()
     {
         $this->register( \Navel\Foundation\Routing\Router::class );
     }
 
+    /**
+     * [register description]
+     *
+     * @param  [type]  $provider [description]
+     * @param  boolean $force    [description]
+     * @return [type]            [description]
+     */
     private function register( $provider, $force = false )
     {
         // If the provider is a string, we will resolve it
@@ -53,21 +100,43 @@ class Application extends Container
         }
     }
 
+    /**
+     * [bootProvider description]
+     *
+     * @param  [type] $provider [description]
+     * @return [type]           [description]
+     */
     public function bootProvider( $provider )
     {
         return $provider;
     }
 
+    /**
+     * [resolveProvider description]
+     *
+     * @param  [type] $provider [description]
+     * @return [type]           [description]
+     */
     public function resolveProvider( $provider )
     {
         return new $provider( $this );
     }
 
+    /**
+     * [isBooted description]
+     *
+     * @return boolean [description]
+     */
     public function isBooted()
     {
         return $this->booted ?? null;
     }
 
+    /**
+     * [setBasePath description]
+     * 
+     * @param [type] $base_path [description]
+     */
     private function setBasePath( $base_path )
     {
         $base_path = rtrim( $base_path );
